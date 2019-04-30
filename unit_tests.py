13fc3153334ac79d4283a4ed40e0d689df8ed3b2
ssalen1 <47227356+ssalen1@users.noreply.github.com>
2019-04-29 20:44:15 -0400

    Added some docstrings - needs more

--- unit_tests.py
+++ unit_tests.py
@@ -1,14 +1,37 @@
+'''
+Authors: Kunal Dani, Marina Morrow, Shyanne Salen,
+Last Modified: April 29th, 2019
+Snake Project
+Software Carpentry
+Classic Snake
+
+This file unit tests our functions in our snake game to see where the 
+errors occur.
+
+***CLASS***
+	Test_Snake
+		This function test our snake game.
+'''
+
 from pygame.locals import *
 import pygame, sys
 import random
 
 class Test_Snake(object):
 	'''
+	This class contains the unit tests to test our functions.
 	'''
 	def get_colors_UT(self, colors):
 		'''
+		This function tests to see if get_colors returns a 
+		dictionary.
+
+		***Parameter***
+			colors: *dict*
+				Dictionary from the get colors function.
 		'''
-		assert (type(colors) == dict), "The colors are not a dictionary"
+		assert (type(colors) == dict), "The colors are not a " \
+		"dictionary"
 
 	def hit_random_snake_UT(self, hit_random_snake):
 		'''
@@ -22,11 +45,15 @@ class Test_Snake(object):
 		'''
 		'''
 		for i in range(random_number_rotten):
-			if (snake.x[0], snake.y[0]) == (rotten_apples[i].x, rotten_apples[i].y):
-				assert hit_rotten_apple == True, 'Did not actually hit apple'
+			if (snake.x[0], snake.y[0]) == (rotten_apples[i].x, \
+				rotten_apples[i].y):
+				assert hit_rotten_apple == True, 'Did not actually' \
+				' hit apple'
 
 	def block_size_UT(self, block_size, game_height, game_width):
 		'''
 		'''
-		assert game_height % block_size == 0, 'game height is not divisible by block_size'
-		assert game_width % block_size == 0, 'game width is not divisible by block_size'
+		assert game_height % block_size == 0, 'game height is not divisible' \
+		' by block_size'
+		assert game_width % block_size == 0, 'game width is not divisible by' \
+		' block_size'
